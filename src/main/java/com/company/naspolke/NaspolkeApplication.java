64b3a0dfa36572e6e35649b82d0aff4d7003e371
kuba-bogacki <jb.bogacki@gmail.com>
2022-06-01 10:39:05 +0200

    starting project

---
+++ src/main/java/com/company/naspolke/NaspolkeApplication.java
@@ -0,0 +1,13 @@
+package com.company.naspolke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaspolkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaspolkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/naspolke/NaspolkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.naspolke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaspolkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
