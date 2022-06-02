040120c5cc40297d5441372a5cb285ef92f118b1
BartoszKosicki <89380356+BartoszKosicki@users.noreply.github.com>
2022-06-02 12:15:15 +0200

    add krs number user input and validation

--- src/main/webapp/na-spolke-client/src/App.js
+++ src/main/webapp/na-spolke-client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AddCompany from "./components/addCompany/AddCompany";
 
 function App() {
   return (
     <div className="App">
-        Hello Client
+        <AddCompany/>
     </div>
   );
 }
---
+++ src/main/webapp/na-spolke-client/src/components/addCompany/AddCompany.js
@@ -0,0 +1,10 @@
+import KrsUserForm from "./KrsUserForm";
+
+const AddCompany = ()=>{
+
+    return <div>
+        <KrsUserForm/>
+    </div>
+}
+
+export default AddCompany;
