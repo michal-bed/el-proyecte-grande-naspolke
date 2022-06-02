9ca915ca37ac9036bcd8d7e19f4570889e433cee
dagggy <krzysiekztubow@gmail.com>
2022-06-02 11:16:49 +0200

    If applied, this commit will make the Navbar fully dynamic and modular

--- src/main/webapp/na-spolke-client/src/components/Navbar/Navbar.js
+++ src/main/webapp/na-spolke-client/src/components/Navbar/Navbar.js
@@ -1,28 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {NavbarItemsLeft, NavbarItemsRight} from './NavbarItems/NavbarItems.js';
 import './Navbar.css';
 
 
 export default function Navbar() {
+
+    const [height, setHeight] = useState(0);
+    const ref = useRef(null);
+
     const [stickyClass, setStickyClass] = useState('');
     useEffect(() => {
+        setHeight(ref.current.clientHeight);
+
         window.addEventListener('scroll', stickyNavbar);
         return () => window.removeEventListener('scroll', stickyNavbar)
     }, [])
 
     const stickyNavbar = () => {
         if (window !== undefined) {
-            window.scrollY > document.getElementById('navbar').offsetTop ?
+            const navbar = document.getElementById('nav-wrapper');
+            const sticky = navbar.offsetTop;
+
+            window.scrollY > sticky ?
                 setStickyClass('sticky-nav') : setStickyClass('');
         }
     }
 
     return(
         <header>
-            <div>logo space</div>
-            <div id={'navbar'} className={stickyClass}>
-                <NavbarItemsLeft />
-                <NavbarItemsRight />
+            <div>
+                <p>logo</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+
+            </div>
+            <div ref={ref} id={'nav-wrapper'} style={{minHeight: height}}>
+                <div id={'navbar'} className={stickyClass}>
+                    <NavbarItemsLeft />
+                    <NavbarItemsRight />
+                </div>
             </div>
         </header>
     )
--- src/main/webapp/na-spolke-client/src/components/Navbar/NavbarItems/NavbarItems.js
+++ src/main/webapp/na-spolke-client/src/components/Navbar/NavbarItems/NavbarItems.js
@@ -34,11 +34,11 @@ const NavbarItems = ({itemsArray}) => {
     for (let i = 0; i < itemsArray.length; i++) {
         if (i === itemsArray.length-1) {
             items.push(
-                <a className={'nav-button'} href={itemsArray[i][1]}>{itemsArray[i][0]}</a>
+                <a key={itemsArray[i][0]} className={'nav-button nav-animation'} href={itemsArray[i][1]}>{itemsArray[i][0]}</a>
             )
         } else {
             items.push(
-                <a className={'nav-button right-border'} href={itemsArray[i][1]}>{itemsArray[i][0]}</a>
+                <a key={itemsArray[i][0]} className={'nav-button nav-animation right-border'} href={itemsArray[i][1]}>{itemsArray[i][0]}</a>
             )
         }
     }
