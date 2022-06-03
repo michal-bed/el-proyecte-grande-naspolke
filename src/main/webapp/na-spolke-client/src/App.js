8d03360ddf35a3765c245f5c9adaf0767238f457
BartoszKosicki <89380356+BartoszKosicki@users.noreply.github.com>
2022-06-03 02:00:52 +0200

    code refactor - add new main page component

--- src/main/webapp/na-spolke-client/src/App.js
+++ src/main/webapp/na-spolke-client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Registration from "./components/registration/Registration";
 import AddCompany from "./components/addCompany/AddCompany";
+import MainPage from "./components/mainPage/mainPage";
 
 function App() {
   return (
     <div className="App">
+        <MainPage/>
         <Registration />
         <AddCompany/>
     </div>
