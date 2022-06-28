b8e3318a32ee1004f8f359e20126c20c8e4569e2
Macbook Michał Rogalski <micha@MacBook-Pro-micha.local>
2022-06-28 14:19:14 +0200

    Change way text is colored in footer

--- src/main/webapp/na-spolke-client/src/componentsInUse/footer/Footer.js
+++ src/main/webapp/na-spolke-client/src/componentsInUse/footer/Footer.js
@@ -11,42 +11,42 @@ const WhiteTextTypography = withStyles({
 export default function Footer() {
     return <footer>
         <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }}  bgcolor="text.secondary">
-            <WhiteTextTypography>
+
             <Container maxWidth="lg">
                 <Grid container spacing={5}>
                     <Grid item xs={12} sm={4}>
-                        <Box color='white' borderBottom={1}>Kontakt</Box>
+                        <Box borderBottom={1}><WhiteTextTypography>Kontakt</WhiteTextTypography></Box>
                         <Box>
-                            E-mail:
+                            <WhiteTextTypography>E-mail:</WhiteTextTypography>
                         </Box>
                         <Box>
-                            naspolke@gmail.com
+                            <WhiteTextTypography>naspolke@gmail.com</WhiteTextTypography>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={4}>
-                        <Box color='white' borderBottom={1}>Informacje</Box>
+                        <Box borderBottom={1}><WhiteTextTypography>Informacje</WhiteTextTypography></Box>
                         <Box>
                             <Link href="/#o_nas" color="inherit">
-                                O nas
+                                <WhiteTextTypography>O nas</WhiteTextTypography>
                             </Link>
                         </Box>
                         <Box>
                             <Link href="/#uslugi">
-                                Usługi
+                                <WhiteTextTypography>Usługi</WhiteTextTypography>
                             </Link>
                         </Box>
                         <Box>
                             <Link href="/faq">
-                                FAQ
+                                <WhiteTextTypography>FAQ</WhiteTextTypography>
                             </Link>
                         </Box>
                     </Grid>
                 </Grid>
                 <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
-                    Na spółkę &reg; {new Date().getFullYear()}
+                    <WhiteTextTypography> Na spółkę &reg; {new Date().getFullYear()}</WhiteTextTypography>
                 </Box>
             </Container>
-            </WhiteTextTypography>
+
         </Box>
     </footer>
 }
